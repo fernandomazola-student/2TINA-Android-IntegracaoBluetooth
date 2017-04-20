c8d7d271aa9602d8c5cf10a53369095c57a9bf36
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-20 10:51:07 -0300

    Adicionado a logica para ativar o bluetooth e retornando as listas de dispositivos pareados no oncreate

--- app/src/main/java/br/com/fiap/a2tina_android_integracaobluetooth/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_integracaobluetooth/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.fiap.a2tina_android_integracaobluetooth;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -32,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         adapter.add("Selecione um dispositivo");
+
+        if (bluetooth != null)
+        {
+
+            //Se o bluetooth não estiver ativo, mandar ativar.
+            if (!bluetooth.isEnabled())
+            {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                int REQUEST_ENABLE_BT = 1;
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+
+            dispositivosPareados = bluetooth.getBondedDevices();
+
+            if ( dispositivosPareados.size() > 0 )
+            {
+                for (BluetoothDevice item : dispositivosPareados)
+                {
+                    adapter.add(item.getName());
+                }
+            }
+        }
+        spDispositivos.setAdapter(adapter);
+
 
     }
 
